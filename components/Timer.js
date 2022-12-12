6881fb7e13971f4f4fd1dd66c5cfe42a56a83056
kristophesankar <kristophe.lukas.sankar@gmail.com>
2022-12-12 15:50:45 -0400

    add: add xstate initial state machine

--- components/Timer.js
+++ components/Timer.js
@@ -1,5 +1,19 @@
-import {timer, btn} from '@/styles/Index.module.sass'
-export default function Timer () {
+import { timer, btn } from '@/styles/Index.module.sass'
+import { useMachine } from '@xstate/react'
+import { createMachine } from 'xstate'
+
+const timerMachine = createMachine({
+  id: 'timer',
+  initial: 'focus',
+  states: {
+    focus: {},
+    shortBreak: {},
+    longBreak: {},
+    done: {},
+  }
+})
+
+export default function Timer() {
   return (
     <div className={timer}>
       <h1 className={timer}>25:00</h1>
