929c8a10d9e23e1b035de8aa2bb4205f191d3d24
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-06-23 20:07:26 -0400

    fix linting errors & removed component tests

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,5 @@
 import { card } from '@/styles/Index.module.sass'
 import Timer from '@/components/Timer'
-import TimerNav from '@/components/TimerNav'
 export default function Card() {
   return (
     <div className={card}>
--- components/Content.js
+++ components/Content.js
@@ -1,4 +1,4 @@
-import { item, content } from '../styles/Index.module.sass'
+import { item } from '../styles/Index.module.sass'
 import Card from './Card'
 export default function Content() {
   return <div className={item}><Card /></div>
