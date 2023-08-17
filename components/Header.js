8c851deabe4078a7789961fd8ce216ce10a3bda5
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-08-17 12:10:08 -0400

    bugfix: change class to className

--- components/Header.js
+++ components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <div className={item}>
       <div className={header}>
         <Image src="/assets/images/dorotimer-logo.svg" width={0} height={0} style={{ width: '50%', height: 'auto' }}/>
-        <h1 class={sitename}>pomolotl</h1>
+        <h1 className={sitename}>pomolotl</h1>
       </div>
     </div>
   )
