48d72e3ffcfa4d5ae71145e30f883538f88412bc
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-08-17 12:07:27 -0400

    bugfix: replace img tag with Image

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import { item, header, sitename } from '../styles/Index.module.sass'
+import Image from 'next/image'
 export default function Header() {
   return (
     <div className={item}>
       <div className={header}>
-        <img src="/assets/images/dorotimer-logo.svg" style={{ width: '50%', height: 'auto' }}/>
+        <Image src="/assets/images/dorotimer-logo.svg" width={0} height={0} style={{ width: '50%', height: 'auto' }}/>
         <h1 class={sitename}>pomolotl</h1>
       </div>
     </div>
