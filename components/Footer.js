133df5b2364c6dcd428426970e9c3086683f902e
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-08-17 12:01:49 -0400

    change name

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import { item, footer } from '../styles/Index.module.sass'
 export default function Footer() {
   return (
     <div className={item}>
-      <div className={footer}>axotimer.dev</div>
+      <div className={footer}>pomolotl.dev</div>
     </div>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     <div className={item}>
       <div className={header}>
         <img src="/assets/images/dorotimer-logo.svg" style={{ width: '50%', height: 'auto' }}/>
-        <h1 class={sitename}>&bull; axotimer &bull;</h1>
+        <h1 class={sitename}>pomolotl</h1>
       </div>
     </div>
   )
