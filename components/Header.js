b731559a74f925a39c4a2a46be4456caf3df2e65
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-08-17 12:15:25 -0400

    format code

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,13 @@ export default function Header() {
   return (
     <div className={item}>
       <div className={header}>
-        <Image src="/assets/images/dorotimer-logo.svg" width={0} height={0} style={{ width: '50%', height: 'auto' }}/>
+        <Image
+          src="/assets/images/dorotimer-logo.svg"
+          alt="pomolotl logo"
+          width={0}
+          height={0}
+          style={{ width: '50%', height: 'auto' }}
+        />
         <h1 className={sitename}>pomolotl</h1>
       </div>
     </div>
