ec1ae4245e65a913887dc4e25edc422ed2b80adc
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-08-18 09:41:06 -0400

    fixed some formatting issues for prettier

--- components/Help.js
+++ components/Help.js
@@ -6,12 +6,12 @@ export default function Help() {
       <div className={help}>
         <h3>How to use this app.</h3>
         <p>
-          Press the "quot;Focus"quot; button followed by start to enter a focus sprint. At
-          the end of the timer a sound will indicate this sprint is over and
-          will automatically move to the next logical state. In this
-          implimentation a "quot;Short Break"quot; follows the completition of the "quot;Focus"quot;
-          state three times before finally entering a "quot;Long Break"quot; state. The
-          pattern is then repeated.
+          Press the &quot;Focus&quot; button followed by start to enter a focus
+          sprint. At the end of the timer a sound will indicate this sprint is
+          over and will automatically move to the next logical state. In this
+          implimentation a &quot;Short Break&quot; follows the completition of
+          the &quot;Focus&quot; state three times before finally entering a
+          &quot;Long Break&quot; state. The pattern is then repeated.
         </p>
         <h3>What does pomolotl mean?</h3>
         <p>Pomolotl is just a combination of two words. Pomodoro & Axolotl.</p>
@@ -20,8 +20,9 @@ export default function Help() {
         <p>
           Pomodoro is a time management technique developed by Francesco Cirillo
           in the late 1980s. The technique involves breaking work into focused
-          intervals of 25 minutes, called "quot;Pomodoros,"quot; separated by short breaks
-          of 5-10 minutes. After four Pomodoros, a longer break of 15-30 minutes
+          intervals of 25 minutes, called &quot;Pomodoros,&quot; separated by
+          short breaks of 5-10 minutes. After four Pomodoros, a longer break of
+          15-30 minutes.
         </p>
         <p>
           The idea behind the Pomodoro technique is that by breaking work into
@@ -30,10 +31,10 @@ export default function Help() {
           regular breaks to rest and recharge.
         </p>
         <p>
-          The name "quot;Pomodoro"quot; comes from the Italian word for tomato, which is
-          what Cirillo used as a timer when he first developed the technique.
-          Today, there are many digital Pomodoro timers available, making it
-          easy to implement the technique in your work routine.
+          The name &quot;Pomodoro&quot; comes from the Italian word for tomato,
+          which is what Cirillo used as a timer when he first developed the
+          technique. Today, there are many digital Pomodoro timers available,
+          making it easy to implement the technique in your work routine.
         </p>
       </div>
     </div>
