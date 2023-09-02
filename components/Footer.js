d3d2e2726adf589f0326e082b95e74e315be782b
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-09-02 19:13:26 -0400

    add font files

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import { item, footer } from '../styles/Index.module.sass'
 export default function Footer() {
   return (
     <div className={item}>
-      <div className={footer}>pomolotl.dev</div>
+      <div className={footer}>pomolotl<br/></div>
     </div>
   )
 }
