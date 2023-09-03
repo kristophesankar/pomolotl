b6061bd7eb96f37ced11fee7ae1051f6f28bcfef
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-09-03 14:15:18 -0400

    add: create users service

---
+++ db/mongodb.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+}
+
+let client
+let clientPromise
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid environment variable: "MONGODB_URI"')
+}
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    global._mongoClientPromise = client.connect()
+  }
+  clientPromise = global._mongoClientPromise
+} else {
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+
+export default clientPromise
---
+++ pages/api/users.js
@@ -0,0 +1,27 @@
+import clientPromise from '../../db/mongodb'
+
+// handler that serves users
+export default async function handler(req, res) {
+  const client = await clientPromise
+  const db = client.db('pomolotltest')
+  switch (req.method) {
+    case 'GET':
+      const users = await db.collection('Users').find({}).toArray()
+      res.json({ status: 200, data: users })
+      break
+  }
+}
+
+export async function getServerSideProps(context) {
+  let res = await fetch('http://localhost:3000/api/users', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  let users = await res.json()
+
+  return {
+    props: { users },
+  }
+}
