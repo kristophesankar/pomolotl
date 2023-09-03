15b6a628263dd8c79c058dd81601844bfc6ef3b6
Kristophe Sankar <kristophe.lukas.sankar@gmail.com>
2023-09-03 14:22:22 -0400

    chore: move context state providers to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { useState, useMemo } from 'react'
+import { container } from '@/styles/Index.module.sass'
+import { GlobalStateProvider } from '@/providers/globalState'
+import { PageContext } from '@/providers/helpProvider'
 import '@/styles/globals.sass'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [page, setPage] = useState('timer')
+  const value = useMemo(() => ({ page, setPage }), [page])
+
+  return (
+    <GlobalStateProvider>
+      <PageContext.Provider value={value}>
+        <div className={container}>
+          {useMemo(
+            () => (
+              <>
+                <Component {...pageProps} />
+              </>
+            ),
+            []
+          )}
+        </div>
+      </PageContext.Provider>
+    </GlobalStateProvider>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,13 @@
-import { useState, useMemo } from 'react'
-import { container } from '@/styles/Index.module.sass'
 import Header from '@/components/Header'
 import Content from '@/components/Content'
 import Footer from '@/components/Footer'
-import { GlobalStateProvider } from '@/providers/globalState'
-import { PageContext } from '@/providers/helpProvider'
 
 export default function Home() {
-  const [page, setPage] = useState('timer')
-  const value = useMemo(() => ({ page, setPage }), [page])
   return (
-    <GlobalStateProvider>
-      <PageContext.Provider value={value}>
-        <div className={container}>
-          {useMemo(
-            () => (
-              <>
-                <Header />
-                <Content />
-                <Footer />
-              </>
-            ),
-            []
-          )}
-        </div>
-      </PageContext.Provider>
-    </GlobalStateProvider>
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
   )
 }
